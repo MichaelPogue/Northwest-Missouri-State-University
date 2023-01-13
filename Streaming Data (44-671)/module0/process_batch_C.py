c82cc49c03f93246c6ba5f107aa70c4ae03390fa
Michael Pogue <115908053+MichaelPogue@users.noreply.github.com>
2023-01-12 19:48:38 -0600

    final update maybe

--- Streaming Data (44-671)/module0/process_batch_C.py
+++ Streaming Data (44-671)/module0/process_batch_C.py
@@ -1,72 +1,84 @@
+# Module 0: Homework 1 - By Michael Pogue
+
 import os
 import csv
 import pandas as pd
-
-
 
 BASE_FILE = 'batchfile_0_farenheit.csv'
 input_file_name = 'batchfile_2_kelvin.csv'
 output_file_name = 'batchfile_3_farenheit.csv'
 
-input_file_name = "batchfile_0_farenheit.csv"
-output_file_name = "batchfile_1_celcius.csv"
-input_file = open(input_file_name, "r")
-output_file = open(output_file_name, "w", newline='')
+class tempConver:
+    def __init__(self):
+        self.BASE_FILE = BASE_FILE
 
-reader = csv.reader(input_file, delimiter=",")
-writer = csv.writer(output_file, delimiter=",")
+    def convert_far_cel(self):
+        input_file_name = "batchfile_0_farenheit.csv"
+        output_file_name = "batchfile_1_celcius.csv"
+        input_file = open(input_file_name, "r")
+        output_file = open(output_file_name, "w", newline='')
 
-header = next(reader)
+        reader = csv.reader(input_file, delimiter=",")
+        writer = csv.writer(output_file, delimiter=",")
 
-header_list = ["Year","Month","Day","Time","TempC"]
-writer.writerow(header_list)
+        header = next(reader)
 
-for row in reader:
-    Year, Month, Day, Time, TempA = row
-    TempB = round((float(TempA) - 32.0) * 5.0 / 9.0,2)
-    writer.writerow([Year, Month, Day, Time, TempB])
+        header_list = ["Year","Month","Day","Time","TempC"]
+        writer.writerow(header_list)
 
-output_file.close()
-input_file.close()
+        for row in reader:
+            Year, Month, Day, Time, TempA = row
+            TempB = round((float(TempA) - 32.0) * 5.0 / 9.0,2)
+            writer.writerow([Year, Month, Day, Time, TempB])
 
-input_file_name = "batchfile_1_celcius.csv"
-output_file_name = "batchfile_2_kelvin.csv"
-input_file = open(input_file_name, "r")
-output_file = open(output_file_name, "w", newline='')
+        output_file.close()
+        input_file.close()
 
-reader = csv.reader(input_file, delimiter=",")
-writer = csv.writer(output_file, delimiter=",")
+    def convert_cel_kel(self):
+        input_file_name = "batchfile_1_celcius.csv"
+        output_file_name = "batchfile_2_kelvin.csv"
+        input_file = open(input_file_name, "r")
+        output_file = open(output_file_name, "w", newline='')
 
-header = next(reader)
+        reader = csv.reader(input_file, delimiter=",")
+        writer = csv.writer(output_file, delimiter=",")
 
-header_list = ["Year","Month","Day","Time","TempK"]
-writer.writerow(header_list)
+        header = next(reader)
 
-for row in reader:
-    Year, Month, Day, Time, TempA = row
-    TempB = round((float(TempA) +273.15),2)
-    writer.writerow([Year, Month, Day, Time, TempB])
+        header_list = ["Year","Month","Day","Time","TempK"]
+        writer.writerow(header_list)
 
-output_file.close()
-input_file.close()
+        for row in reader:
+            Year, Month, Day, Time, TempA = row
+            TempB = round((float(TempA) +273.15),2)
+            writer.writerow([Year, Month, Day, Time, TempB])
 
-input_file_name = "batchfile_2_kelvin.csv"
-output_file_name = "batchfile_3_farenheit.csv"
-input_file = open(input_file_name, "r")
-output_file = open(output_file_name, "w", newline='')
+        output_file.close()
+        input_file.close()
 
-reader = csv.reader(input_file, delimiter=",")
-writer = csv.writer(output_file, delimiter=",")
+    def convert_kel_far(self):
+        input_file_name = "batchfile_2_kelvin.csv"
+        output_file_name = "batchfile_3_farenheit.csv"
+        input_file = open(input_file_name, "r")
+        output_file = open(output_file_name, "w", newline='')
 
-header = next(reader)
+        reader = csv.reader(input_file, delimiter=",")
+        writer = csv.writer(output_file, delimiter=",")
 
-header_list = ["Year","Month","Day","Time","TempF"]
-writer.writerow(header_list)
+        header = next(reader)
 
-for row in reader:
-    Year, Month, Day, Time, TempA = row
-    TempB = round(((float(TempA)* 9/5) - 459.67),2)  
-    writer.writerow([Year, Month, Day, Time, TempB])
+        header_list = ["Year","Month","Day","Time","TempF"]
+        writer.writerow(header_list)
 
-output_file.close()
-input_file.close()
+        for row in reader:
+            Year, Month, Day, Time, TempA = row
+            TempB = round(((float(TempA)* 9/5) - 459.67),2)  
+            writer.writerow([Year, Month, Day, Time, TempB])
+
+        output_file.close()
+        input_file.close()
+
+tc = tempConver
+tc.convert_far_cel(BASE_FILE)
+tc.convert_cel_kel(BASE_FILE)
+tc.convert_kel_far(BASE_FILE)
